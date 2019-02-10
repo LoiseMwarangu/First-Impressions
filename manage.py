1b986f5efa223faa3c831b83ec6c8d546a3c06d7
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-10 17:55:55 +0300

    imported classes to shell context

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User
+from app.models import Role
 from flask_migrate import Migrate, MigrateCommand
 
 
