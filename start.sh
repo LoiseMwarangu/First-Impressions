affe24e47a42bdeda4e8eb9aa0bf341dab6ea683
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-15 19:11:40 +0300

    corrected password

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='Flask WTF Secret Key'
 
 export EMAIL_USERNAME='loisemwarangu@gmail.com'
-export EMAIL_PASSWORD='loise@9954'
+export EMAIL_PASSWORD='loise1993'
 
 python3.6 manage.py server
