c096cce60dbe19dee0118b47925061dea6ea5762
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-16 13:33:03 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 export SECRET_KEY='Flask WTF Secret Key'
 
 export EMAIL_USERNAME='loisemwarangu@gmail.com'
-export EMAIL_PASSWORD='loise1993'
+export EMAIL_PASSWORD='loise@9954'
 
 python3.6 manage.py server
+(virtual)$ heroku config:set MAIL_USERNAME='loisemwarangu@gmail.com'
+(virtual)$ heroku config:set MAIL_PASSWORD='loise@9954'
