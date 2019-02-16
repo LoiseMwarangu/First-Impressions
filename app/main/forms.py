ea2bce4e9090553a0ebd649055cd510e51dc07c0
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-16 13:03:43 +0300

    fixed errors

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
 
@@ -7,11 +7,11 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Add New Pitch',validators = [Required()])
     submit = SubmitField('Submit')
 
-# class PitchForm(FlaskForm):
-#     title = StringField('Title', validators = [Required()])
-#     text = TextAreaField('Pitch',validators = [Required()])
-#     category = SelectField('Category', choices = [('pickup lines', 'Pickup lines'),('interview','Interview'), ('product','Product'),('promotion','Promotion')], validators = [Required()])
-#     submit = SubmitField('Post')
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators = [Required()])
+    text = TextAreaField('Pitch',validators = [Required()])
+    category = SelectField('Category')#'),('promotion','Promotion')], validators = [Required()])
+    submit = SubmitField('Post')
 
 class CommentForm(FlaskForm):
     text = TextAreaField('Leave a comment:',validators=[Required()])
