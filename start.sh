6603205e27e7e4e252336c1ba224a52925922203
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-17 16:15:16 +0300

    ignored start

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='Flask WTF Secret Key'
 
 export EMAIL_USERNAME='loisemwarangu@gmail.com'
-export EMAIL_PASSWORD='loise@9954'
+export EMAIL_PASSWORD='loise1993'
 export FLASK_DEBUG=1
 export FLASK_APP=app
 python3.6 manage.py server
